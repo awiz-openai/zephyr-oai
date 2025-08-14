9f775c33746a90a83308dd7efb9ae94b1f550f08
Aaron Wisner <awiz@openai.com>
2025-08-14 16:29:49 +0000

    Replace ~ when creating library names from path
    Signed-off-by: Aaron Wisner <awiz@openai.com>

--- cmake/modules/extensions.cmake
+++ cmake/modules/extensions.cmake
@@ -446,6 +446,9 @@ macro(zephyr_library_get_current_dir_lib_name base lib_name)
   # Replace / with __ (driver/serial => driver__serial)
   string(REGEX REPLACE "/" "__" name ${name})
 
+  # Replace ~ with - (driver/~serial => driver-serial)
+  string(REGEX REPLACE "~" "-" name ${name})
+
   # Replace : with __ (C:/zephyrproject => C____zephyrproject)
   string(REGEX REPLACE ":" "__" name ${name})
 
